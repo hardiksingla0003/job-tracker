4a95934a5e3951ec029ec4d361c4a8d85a0eabb2
Hardik Singla <hardiksingla1999@gmail.com>
2026-02-26 19:43:03 +0530

    change in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import JobProvider from "./context/JobContext.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <JobProvider>
         <App />
       </JobProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 );
